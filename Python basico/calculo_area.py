b927c0790b3293d1c524d30a766780175091739f
Guilherme Ferreira Costa <guilhermeferreira1749@gmail.com>
2025-09-25 23:48:12 -0300

    Delete Python basico directory

--- Python basico/calculo_area.py
+++
@@ -1,6 +0,0 @@
-PI = 3.14159
-
-raio = 5
-area = PI * (raio ** 2)
-
-print("Área do circulo: ", area)
--- Python basico/saida_dados.py
+++
@@ -1,9 +0,0 @@
-nome = "João"
-idade = 25
-altura = 1.75
-
-# Formatação usando f-strings:
-saida_formatada = f"Nome: {nome}, Idade: {idade}, Altura: {altura:.2f}m"
-
-# Exibir a saída formatada:
-print(saida_formatada)
